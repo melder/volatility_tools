da605136afccd38df3c6f7b759b08cc52a4ca193
melder <melder@fuccboi.wtf>
2023-10-02 05:43:42 +0000

    some changes to run site live

--- cache_autocomplete.py
+++ cache_autocomplete.py
@@ -3,18 +3,23 @@ redis caching functions for search autocomplete
 """
 
 import cache_datapoints as cd
+from helpers import csv_helpers
 
 _AUTOCOMPLETE_KEY = cd.key_join(cd.IV_DATA_NAMESPACE, "autocomplete")
 
 def parse_members_from_iv_history_zset():
-    keys = cd.r.scan_iter(f"*{cd.AVERAGE_NAMESPACE}*")
-    return [x.split(":")[-1] for x in keys]
+    return csv_helpers.fetch_tickers_from_csv("csv/options.csv")
+    #keys = cd.r.scan_iter(f"*{cd.AVERAGE_NAMESPACE}*")
+    #return [x.split(":")[-1] for x in keys]
 
 def purge_autocomplete():
     return cd.r.delete(_AUTOCOMPLETE_KEY)
 
 def insert_to_set(*members):
     cd.r.sadd(_AUTOCOMPLETE_KEY, *members)
+
+def blacklisted_tickers():
+    return config.redis_client().hgetall("iv_scraper:blacklist").keys()
 
 def get_autocomplete_members():
     members = cd.r.smembers(_AUTOCOMPLETE_KEY)
